fecd2312c6b4cb83a6312c9f1077a74b44efb13b
kammaii <kammaii@naver.com>
2023-08-14 00:10:40 +0900

    fix: functions
    - podo message 버그 수정
    - sendgrid로 이메일 전송 시도했으나 실패

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,63 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+
 
 // export GOOGLE_APPLICATION_CREDENTIALS="G:\keys\podo-49335-firebase-adminsdk-qqve9-4227c667f7.json"
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  //databaseURL: "https://podo-705c6.firebaseio.com"
-  databaseURL: "http://localhost:4000/firestore"
-});
+admin.initializeApp();
 
-let messaging = admin.messaging();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-function sendMessage(token, payload) {
-  return messaging.sendToDevice(token, payload)
-    .then(function(response){
-      console.log('Notification sent successfully:',response);
-      return response;
+function onFeedbackSent(snap, context) {
+  const feedbackData = snap.data();
+  const userEmail = feedbackData.userEmail;
+  const message = feedbackData.message;
+  const mailOptions = {
+    from: 'akorean.help@gmail.com',
+    to: 'akorean.help@gmail.com', // 수신 이메일 주소
+    subject: '[podo] Feedback from the user',
+    text: '${message}\n\n${userEmail}',
+  };
+
+  return sgMail.send(mailOptions)
+    .then(() => {
+      console.log('이메일 전송 성공');
+      return null;
     })
-    .catch(function(error){
-      console.log('Notification sent failed:',error);
+    .catch((error) => {
+      console.error('이메일 전송 실패:', error);
+      return null;
     });
 }
 
 
-function onCloudMsgActive(change, context) {
+function onPodoMsgActivated(change, context) {
+   let beforeData = change.before.data();
    let afterData = change.after.data();
-   if(afterData.isActive) {
-       //todo: send fcmMsg to users who have accepted fcm. (or 베스트 리플라이 선정 끝나면 모두에게 보내기?)
+   if(!beforeData.isActive && afterData.isActive) {
+     const payload = {
+       notification: {
+         title: 'podo',
+         body: afterData.title['ko'],
+       }
+     };
+
+     return admin.messaging().sendToTopic('allUsers', payload)
+       .then((response) => {
+         console.log('알림 전송 성공:', response);
+         return null;
+       })
+       .catch((error) => {
+         console.log('알림 전송 실패:', error);
+       });
    }
+   return null;
 }
 
-function onWritingReply(change, context) {
+function onWritingReplied(change, context) {
   console.log('!!!!! Writing has replied !!!!!');
-  console.log(context.params.writingId);
-  console.log(context.eventType);
-  console.log('BEFORE:');
-  console.log(change.before.data());
-  console.log('AFTER:');
-  console.log(change.after.data());
   let afterData = change.after.data();
   let status = afterData.status;
   let guid = afterData.guid;
@@ -46,34 +65,34 @@ function onWritingReply(change, context) {
   let fcmToken = afterData.fcmToken;
 
   if(fcmToken != null) {
-      let title;
-      let body = userWriting;
+    let title;
+    let body = userWriting;
 
-      if(status == 1 || status == 2) {
-        title = "Your writing has been reviewed";
-      } else if(status == 3) {
-        title = "Your writing has been returned";
+    if(status == 1 || status == 2) {
+      title = "Your writing has been reviewed";
+    } else if(status == 3) {
+      title = "Your writing has been returned";
+    }
+
+    const payload = {
+      notification: {
+        tag: "writing",
+        title: title,
+        body: body
       }
+    };
 
-      const payload = {
-        notification: {
-          tag: "writing",
-          title: title,
-          body: body
-        }
-      };
-      sendMessage(fcmToken, payload);
+    return admin.messaging().sendToDevice(fcmToken, payload)
+      .then(function(response){
+        console.log('Notification sent successfully:',response);
+        return null;
+      })
+    .catch(function(error){
+      console.log('Notification sent failed:',error);
+    });
   }
-
-  return true;
 }
 
-//Notification sent failed: FirebaseMessagingError: Messaging payload contains an invalid value for the "notification.body" property. Values must be strings.
-
-//Notification sent failed: FirebaseMessagingError: An error occurred when trying to authenticate to the FCM servers.
-//Make sure the credential used to authenticate this SDK has the proper permissions.
-//See https://firebase.google.com/docs/admin/setup for setup instructions. Raw server response: "<HTML>
-
-
-exports.onWritingReply = functions.firestore.document('Writings/{writingId}').onWrite(onWritingReply);
-exports.onCloudMsgActive = functions.firestore.document('CloudMessages/{cloudMessageId}').onWrite(onCloudMsgActive);
+exports.onWritingReply = functions.firestore.document('Writings/{writingId}').onUpdate(onWritingReplied);
+exports.onPodoMsgActive = functions.firestore.document('PodoMessages/{podoMessageId}').onUpdate(onPodoMsgActivated);
+exports.onFeedbackSent = functions.firestore.document('Feedbacks/{feedbackId}').onCreate(onFeedbackSent);
