a7e79b6259764df9cdfa3be1f0a05fe847a5234e
kammaii <kammaii@naver.com>
2023-10-24 23:35:42 +0900

    feat: 활성Basic유저수 그래프 추가

---
+++ lib/ranking.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Ranking extends StatelessWidget {
+  const Ranking({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('랭킹')),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/user/user_main.dart
+++ lib/screens/user/user_main.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:podo_admin/common/database.dart';
 import 'package:podo_admin/common/my_date_format.dart';
+import 'package:podo_admin/ranking.dart';
 import 'package:podo_admin/screens/user/user.dart' as user_info;
 import 'package:podo_admin/screens/user/user_writing_record.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class UserMain extends StatefulWidget {
   UserMain({Key? key, this.userId, this.userEmail}) : super(key: key);
@@ -26,16 +29,17 @@ class _UserMainState extends State<UserMain> {
   int? readingCount;
   int? cloudCount;
   int? flashcardCount;
-  int totalUserCount = 0;
-  int newUserCount = 0;
-  int basicUserCount = 0;
-  int trialUserCount = 0;
-  int premiumUserCount = 0;
-  int newUserPercent = 0;
-  int trialUserPercent = 0;
-  int basicUserPercent = 0;
-  int premiumUserPercent = 0;
-
+  int totalCount = 0;
+  int newCount = 0;
+  int basicCount = 0;
+  int trialCount = 0;
+  int premiumCount = 0;
+  int newPercent = 0;
+  int trialPercent = 0;
+  int basicPercent = 0;
+  int premiumPercent = 0;
+  List<FlSpot> dataPoints = [];
+  int maxActiveCount = 0;
 
   @override
   void initState() {
@@ -46,17 +50,17 @@ class _UserMainState extends State<UserMain> {
       Database().getCount(collection: 'Users', field: 'status', equalTo: 1),
       Database().getCount(collection: 'Users', field: 'status', equalTo: 2),
     ]).then((snapshot) {
-      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
         setState(() {
-          newUserCount = snapshot[0];
-          trialUserCount = snapshot[1];
-          basicUserCount = snapshot[2];
-          premiumUserCount = snapshot[3];
-          totalUserCount = newUserCount + trialUserCount + basicUserCount + premiumUserCount;
-          newUserPercent = (newUserCount / totalUserCount * 100).round();
-          trialUserPercent = (trialUserCount / totalUserCount * 100).round();
-          basicUserPercent = (basicUserCount / totalUserCount * 100).round();
-          premiumUserPercent = (premiumUserCount / totalUserCount * 100).round();
+          newCount = snapshot[0];
+          trialCount = snapshot[1];
+          basicCount = snapshot[2];
+          premiumCount = snapshot[3];
+          totalCount = newCount + trialCount + basicCount + premiumCount;
+          newPercent = (newCount / totalCount * 100).round();
+          trialPercent = (trialCount / totalCount * 100).round();
+          basicPercent = (basicCount / totalCount * 100).round();
+          premiumPercent = (premiumCount / totalCount * 100).round();
         });
       });
     });
@@ -67,8 +71,9 @@ class _UserMainState extends State<UserMain> {
       children: [
         Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
         Text(': ${count.toString()} ', style: const TextStyle(fontSize: 18)),
-        percent != null ?
-        Text('(${percent.toString()}%)', style: const TextStyle(fontSize: 15, color: Colors.grey)) : const Text('      :'),
+        percent != null
+            ? Text('(${percent.toString()}%)', style: const TextStyle(fontSize: 15, color: Colors.grey))
+            : const Text('      :'),
         const SizedBox(width: 30),
       ],
     );
@@ -128,11 +133,149 @@ class _UserMainState extends State<UserMain> {
                   child: const Text('검색'),
                 ),
                 const SizedBox(width: 50),
-                getCountUser('Total', totalUserCount),
-                getCountUser('New', newUserCount, percent: newUserPercent),
-                getCountUser('Trial', trialUserCount, percent: trialUserPercent),
-                getCountUser('Basic', basicUserCount, percent: basicUserPercent),
-                getCountUser('Premium', premiumUserCount, percent: premiumUserPercent),
+                Row(
+                  children: [
+                    getCountUser('Total', totalCount),
+                    getCountUser('New', newCount, percent: newPercent),
+                    getCountUser('Trial', trialCount, percent: trialPercent),
+                    getCountUser('Basic', basicCount, percent: basicPercent),
+                    getCountUser('Premium', premiumCount, percent: premiumPercent),
+                    ElevatedButton(
+                        onPressed: () async {
+                          final now = DateTime.now();
+                          final today = DateTime(now.year, now.month, now.day);
+                          final d_1 = today.subtract(const Duration(days: 1));
+                          final d_2 = today.subtract(const Duration(days: 2));
+                          final d_3 = today.subtract(const Duration(days: 3));
+                          final d_4 = today.subtract(const Duration(days: 4));
+                          final d_5 = today.subtract(const Duration(days: 5));
+                          final query =
+                              FirebaseFirestore.instance.collection('Users').where('status', isEqualTo: 1);
+
+                          final todayCount = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: today)
+                              .where('dateSignIn', isLessThan: today.add(const Duration(days: 1)))
+                              .count()
+                              .get();
+
+                          final d_1Count = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: d_1)
+                              .where('dateSignIn', isLessThan: today)
+                              .count()
+                              .get();
+
+                          final d_2Count = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: d_2)
+                              .where('dateSignIn', isLessThan: d_1)
+                              .count()
+                              .get();
+
+                          final d_3Count = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: d_3)
+                              .where('dateSignIn', isLessThan: d_2)
+                              .count()
+                              .get();
+
+                          final d_4Count = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: d_4)
+                              .where('dateSignIn', isLessThan: d_3)
+                              .count()
+                              .get();
+
+                          final d_5Count = await query
+                              .where('dateSignIn', isGreaterThanOrEqualTo: d_5)
+                              .where('dateSignIn', isLessThan: d_4)
+                              .count()
+                              .get();
+
+                          dataPoints = [
+                            FlSpot(0, d_5Count.count.toDouble()),
+                            FlSpot(1, d_4Count.count.toDouble()),
+                            FlSpot(2, d_3Count.count.toDouble()),
+                            FlSpot(3, d_2Count.count.toDouble()),
+                            FlSpot(4, d_1Count.count.toDouble()),
+                            FlSpot(5, todayCount.count.toDouble()),
+                          ];
+                          List<int> activeCounts = [
+                            todayCount.count,
+                            d_1Count.count,
+                            d_2Count.count,
+                            d_3Count.count,
+                            d_4Count.count,
+                            d_5Count.count
+                          ];
+                          maxActiveCount = activeCounts[0];
+                          for (int count in activeCounts) {
+                            if (count > maxActiveCount) {
+                              maxActiveCount = count;
+                            }
+                          }
+                          final totalActive = activeCounts.reduce((a, b) => a+b);
+
+                          Get.dialog(AlertDialog(
+                            title: Text('최근 5일간 활성 Basic 유저 수:  총 $totalActive 명 (${(totalActive/basicCount*100).roundToDouble()}%)',
+                                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                            content: SizedBox(
+                              height: 300,
+                              width: 400,
+                              child: LineChart(LineChartData(
+                                gridData: const FlGridData(show: false),
+                                titlesData: FlTitlesData(
+                                  topTitles: const AxisTitles(),
+                                  leftTitles: const AxisTitles(),
+                                  rightTitles: const AxisTitles(
+                                      sideTitles: SideTitles(showTitles: true, interval: 10, reservedSize: 50)),
+                                  bottomTitles: AxisTitles(
+                                      sideTitles: SideTitles(
+                                    showTitles: true,
+                                    getTitlesWidget: (x, value) {
+                                      if (x == 0) {
+                                        return const Text('D-5');
+                                      } else if (x == 1) {
+                                        return const Text('D-4');
+                                      } else if (x == 2) {
+                                        return const Text('D-3');
+                                      } else if (x == 3) {
+                                        return const Text('D-2');
+                                      } else if (x == 4) {
+                                        return const Text('D-1');
+                                      } else {
+                                        return const Text('Today');
+                                      }
+                                    },
+                                    interval: 1,
+                                  )),
+                                ),
+                                borderData: FlBorderData(
+                                  show: true,
+                                  border: Border.all(width: 1),
+                                ),
+                                minX: 0,
+                                maxX: 5,
+                                minY: 0,
+                                maxY: ((maxActiveCount / 10).ceil()) * 10 + 10,
+                                lineTouchData: const LineTouchData(
+                                    touchTooltipData: LineTouchTooltipData(tooltipBgColor: Colors.white)),
+                                lineBarsData: [
+                                  LineChartBarData(
+                                    spots: dataPoints,
+                                    color: Colors.deepPurple,
+                                    dotData: const FlDotData(),
+                                  ),
+                                ],
+                              )),
+                            ),
+                          ));
+                        },
+                        child: const Text('활성Basic')),
+                    const SizedBox(width: 30),
+                    ElevatedButton(
+                        onPressed: () {
+                          Get.to(const Ranking());
+                        },
+                        child: const Text('랭킹'))
+                  ],
+                ),
               ],
             ),
             const SizedBox(height: 50),
